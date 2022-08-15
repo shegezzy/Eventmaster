60ff7d73579e7b64fea59fa364db974e681072ca
shegezzy <39660743+shegezzy@users.noreply.github.com>
2022-08-15 23:02:54 +0100

    Add project files.

---
+++ Eventmaster/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventmaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
